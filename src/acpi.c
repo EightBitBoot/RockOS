353632099de95c367ac1901e86687db0059e23aa
Jake <jrs4343@rit.edu>
2024-03-26 13:43:52 -0400

    [acpi] validate RSDP, add SDTH struct

--- src/acpi.c
+++ src/acpi.c
@@ -2,7 +2,14 @@
 #include "debug.h"
 #include "cio.h"
 
-void* _acpi_get_ebda_ptr(void) {
+struct acpi_data {
+	struct acpi_rsdp* rsdp;
+
+	// TODO: how to load and store AML coded tables dynamically?
+};
+static struct acpi_data _acpi_data = {0};
+
+static void* _acpi_get_ebda_ptr(void) {
 	uint16_t *ptr = (uint16_t *) 0x40E;
 
 	void *ret_ptr = (void *) (*ptr << 4);
@@ -12,7 +19,7 @@ void* _acpi_get_ebda_ptr(void) {
 	return ret_ptr;
 }
 
-struct acpi_rsdp* _acpi_get_rsdp_ptr(void) {
+static struct acpi_rsdp* _acpi_find_rsdp_ptr(void) {
 	char *expected_signature = ACPI_RSDP_SIGNATURE;
 
 	// Check Extended BIOS Data Area
@@ -39,19 +46,67 @@ struct acpi_rsdp* _acpi_get_rsdp_ptr(void) {
 			if (*(expected_signature + j) != rsdp->signature[j]) break;
 		}
 
-		// TODO: Never tested since the lab's BIOS does not provide RSDP in EBDA.
 		if (j == sizeof(expected_signature)) return rsdp;
 	}
 
 	return NULL;
 }
 
+static bool_t _acpi_check_rsdp_valid(struct acpi_rsdp* rsdp) {
+	uint8_t actual_checksum;
+
+	// ACPI 1.0 checks
+	actual_checksum = 0;
+	for (int i = 0; i < 20; i++) {
+		actual_checksum += *(((uint8_t *) rsdp) + i);
+	}
+
+	if (actual_checksum != 0) {
+		// rsdp->checksum is a value to ensure the first 20 bytes sum to 0
+		WARNING("[acpi] RSDP checksum is invalid");
+		return false;
+	}
+
+	// ACPI 2.0 checks
+	if (rsdp->revision >= 2) {
+		// >= 2 -> ACPI 2.0
+		// == 0 -> ACPI 1.0
+		// See ACPI Specification (v6.5) Section 5.2.5.3
+
+		actual_checksum = 0;
+		for (int i = 0; i < sizeof(struct acpi_rsdp); i++) {
+			actual_checksum += *(((uint8_t *) rsdp) + i);
+		}
+
+		if (actual_checksum != 0) {
+			// rsdp->extended_checksum is a value to ensure all the bytes in the table sum to 0
+			WARNING("[acpi] RSDP extended checksum is invalid");
+			// TODO: Just set revision to 0 since 1.0 table is fine?
+			return false;
+		}
+	}
+
+	return true;
+}
+
 void _acpi_init( void ) {
 	__cio_printf("[acpi] initialize\n");
 
-	struct acpi_rsdp* rsdp_ptr = _acpi_get_rsdp_ptr();
-	__cio_printf("[acpi] RSDP at 0x%x\n", rsdp_ptr);
-	// TODO: check checksum, store this somewhere?
+	// Find and ensure RSDP table is valid
+	_acpi_data.rsdp = _acpi_find_rsdp_ptr();
+	__cio_printf("[acpi] RSDP at 0x%x\n", _acpi_data.rsdp);
+
+	if (!_acpi_check_rsdp_valid(_acpi_data.rsdp)) {
+		_acpi_data.rsdp = NULL;
+		return;
+	}
+	__cio_printf("[acpi] RSDP valid\n");
+
+	// TODO: FADT, DSDT
+
+	// TODO: ACPI enable (section 16.3.1)
+
+	// TODO: syscalls for changing sleep state (shutdown/reboot/reset/sleep/etc.)
 
 	__cio_printf("[acpi] initialize complete\n");
 }
--- src/acpi.h
+++ src/acpi.h
@@ -4,20 +4,44 @@
 #include "common.h"
 
 #define ACPI_RSDP_SIGNATURE "RSD PTR "
+/**
+ * @struct acpi_rsdp
+ * @brief ACPI Root System Description Pointer
+ * @sa ACPI Specification (v6.5) Section 5.2.5.3
+ */
 struct acpi_rsdp {
 	// ACPI 1.0
-	char signature[8];
+	char signature[8]; // Should be equal to ACPI_RSDP_SIGNATURE
 	uint8_t checksum;
 	char oem_id[6];
 	uint8_t revision;
 	uint32_t rsdt_address;
 
-	// ACPI 2.0 (revision == 2)
+	// ACPI 2.0 (revision >= 2)
 	uint32_t length;
 	uint64_t xsdt_address;
 	uint8_t extended_checksum;
 	uint8_t reserved[3];
-};
+} __attribute__((packed));
+
+/**
+ * @struct acpi_sdt_header
+ * @brief ACPI System Description Table Header
+ * @sa ACPI Specification (v6.5) Section 5.2.6
+ */
+struct acpi_sdt_header {
+	char signature[4];
+	uint32_t length;
+	uint8_t revision;
+	uint8_t checksum;
+
+	char oem_id[6];
+	char oem_table_id[8];
+	uint32_t oem_revision;
+
+	uint32_t creator_id;
+	uint32_t creator_revision;
+} __attribute__((packed));
 
 void _acpi_init(void);
 
