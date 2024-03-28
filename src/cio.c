378575b6ad7de514143500f67d21c9bbae00be19
Seth Teichman (RIT Student) <smt5541@rit.edu>
2024-03-28 17:09:29 -0400

    Actually add the color test to CIO

--- src/cio.c
+++ src/cio.c
@@ -615,12 +615,11 @@ static void __c_keyboard_isr( int vector, int code ) {
 
     int data = __inb( KEYBOARD_DATA );
     int val  = __c_input_scan_code( data );
-    __cio_printf("KB: %d\n", val);
 
-#if TRACING_CONSOLE
 	__cio_printf( "** CIO kbd data 0x%02x val 0x%02x\n",
-			(uint32_t) data, (uint32_t) val );
-#endif
+			(unsigned int) data, (unsigned int) val );
+
+    __vga_text_color_test(val);
 
     // if there is a notification function, call it
     if( val != -1 && __c_notify )
